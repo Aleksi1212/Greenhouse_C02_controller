dc0fb72c754e5278b1c4ccde3005e5b50c4f1519
Aleksi Noro <aleksinoro@gmail.com>
2026-03-02 14:12:23 +0200

    method to send data to thingspeak (NOT TESTED)

--- src/ipstack/thingspeak.cpp
+++ src/ipstack/thingspeak.cpp
@@ -5,22 +5,58 @@ ThingSpeak::ThingSpeak(/*QueueHandle_t _controller_q*/) :
     IPStack(SSID, PW)
     // controller_q(_controller_q)
 {
-    cloud_q = xQueueCreate(10, sizeof(CloudData_t));
+    cloud_q = xQueueCreate(CLOUD_Q_SIZE, sizeof(CloudData_t));
     connect_rc = connect(HTTP_SERVER, 80);
 
-    xTaskCreate(ThingSpeak::send_task, "SEND", 512, static_cast<void *>(this), tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(ThingSpeak::send_task, "SEND", 512, 
+        static_cast<void *>(this), 
+        tskIDLE_PRIORITY + 1, 
+        send_task_handle
+    );
 }
 
 void ThingSpeak::send_task(void *param)
 {
     ThingSpeak *ts = static_cast<ThingSpeak*>(param);
 
+    std::string http_fields = "";
+    char http_msg[512];
+    CloudData_t data;
+
     while (true) {
+        eTaskState task_state = eTaskGetState(ts->send_task_handle);
+
         if (ts->connect_rc == 0) {
-            std::cout << "connected" << std::endl;
-        } else {
-            std::cout << "not connected" << std::endl;
+            if (task_state == eSuspended) vTaskResume(ts->send_task_handle);
+
+            if (xQueueReceive(cloud_q, &data, portMAX_DELAY)) {
+                std::string http_field = "field" + data.field_id + "=" + data.data + "&";
+                http_fields += http_field;
+                continue;
+            }
+            if (http_fields != "") {
+                snprintf(http_msg, 512, SEND_DATA_REQ, THINGSPEAK_WRITE_API_KEY, http_fields.c_str());
+                ts->write(http_msg, 512, 60);
+            }
+            http_fields = "";
+        } else if (ts->connect_rc != 0 && task_state != eSuspended) {
+            vTaskSuspend(NULL)
         }
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
+}
+
+bool ThingSpeak::send_to_queue(std::vector<CloudData_t*> data, TickType_t ticksToWait)
+{
+    int count = 0;
+    for (auto it = data.begin(); it != data.end(); ++it) {
+        if (count == CLOUD_Q_SIZE) break;
+        bool fail xQueueSendToBack(cloud_q, *it, ticksToWait) != pdPASS;
+        if (fail) return false;
+    }
+    return true;
+}
+bool ThingSpeak::send_to_queue(CloudData_t *data, TickType_t ticksToWait)
+{
+    return xQueueSendToBack(cloud_q, data, ticksToWait) == pdPASS;
 }
--- src/ipstack/thingspeak.h
+++ src/ipstack/thingspeak.h
@@ -6,21 +6,54 @@
 #include "task.h"
 #include "queue.h"
 #include "IPStack.h"
+#include <vector>
+
+// struct CloudData_t {
+//     float co2_level;
+//     float rh;
+//     float temp;
+//     float vent_fan_speed;
+//     float co2_set_point;
+// };
+enum FieldID {
+    CO2_LEVEL,
+    REALATIVE_HUMIDITY,
+    TEMPERATURE,
+    VENT_FAN_SPEED,
+    CO2_SET_POINT,
+    // NUM_OF_FIELDS
+}
 
 struct CloudData_t {
-    float co2_level;
-    float rh;
-    float temp;
-    float vent_fan_speed;
-    float co2_set_point;
+    FieldID field_id;
+    float data;
 };
 
-#define HTTP_SERVER "api.thingspeak.com"
+/*
+    Some of these addresses is the thingspeak server ???
+*/
+#define HTTP_SERVER "18.215.51.65"
+// #define HTTP_SERVER "54.158.231.69"
+// #define HTTP_SERVER "api.thingspeak.com"
+
+#define SEND_DATA_REQ \
+    "POST /update.json HTTP/1.1\r\n" \
+    "Host: api.thingspeak.com\r\n" \
+    "User-Agent: PicoW\r\n" \
+    "Accept: */*\r\n" \
+    "Content-Length: 256\r\n" \
+    "Content-Type: application/x-www-form-urlencoded\r\n" \
+    "api_key=%s&%s"
+
+#define CLOUD_Q_SIZE 10
 
 class ThingSpeak : private IPStack {
 private:
     QueueHandle_t cloud_q;
     QueueHandle_t controller_q;
+
+    TaskHandle_t send_task_handle;
+    TaskHandle_t read_task_handle;
 
     int connect_rc = -1;
     int send_rc = -1;
@@ -31,7 +64,8 @@ private:
 
 public:
     ThingSpeak(/*QueueHandle_t _controller_q*/);
-    void send_to_queue(CloudData_t data, TickType_t ticksToWait);
+    bool send_to_queue(std::vector<CloudData_t*> data, TickType_t ticksToWait);
+    bool send_to_queue(CloudData_t *data, TickType_t ticksToWait);
 };
 
 
