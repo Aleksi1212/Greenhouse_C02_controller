f56538d5c3894b405cbfa67642208e85a61a6ef9
reneki <rene.kilpelainen@gmail.com>
2026-03-09 09:46:31 +0200

    let's not break anything

---
+++ src/ui/UITask.h
@@ -0,0 +1,49 @@
+//
+// Created by renek on 04/03/2026.
+//
+
+#ifndef UITASK_H
+#define UITASK_H
+
+#include <memory>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "ssd1306os.h"
+#include "controller/ControllerEnums.h"
+#include "RotaryEncoder.h"
+
+class UITask {
+public:
+    UITask(QueueHandle_t displayQueue, QueueHandle_t controllerQueue);
+
+private:
+    static void runner(void *params);
+    void run();
+
+    void drawHomeScreen();
+    void drawSetpointScreen();
+    void drawWifiMenuScreen();
+    void drawWifiSavedScreen();
+    void drawWifiNewScreen();
+    void handleInput();
+
+    // display is created inside run() after the scheduler starts
+    std::shared_ptr<ssd1306os> display;
+    QueueHandle_t displayQueue;
+    QueueHandle_t controllerQueue;
+    TaskHandle_t handle;
+
+    RotaryEncoder encoder;
+
+    enum class Screen { WIFI_MENU, WIFI_SAVED, WIFI_NEW, HOME, EDIT_SETPOINT };
+    Screen currentScreen{Screen::WIFI_MENU};
+    int wifiMenuSelection{0};  // 0 = Saved login, 1 = Enter new
+
+    int editValue{1000};       // setpoint being edited
+    int currentSetpoint{1000}; // last confirmed setpoint
+
+    sensorData latestData{};   // cached data received from displayQueue
+};
+
+#endif // UITASK_H
