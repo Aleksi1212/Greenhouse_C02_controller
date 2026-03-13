503b852f972f30f8a4ee7c279c8da848dfbb37b9
Aleksi Noro <aleksinoro@gmail.com>
2026-03-13 11:31:49 +0200

    webserver changes

--- webserver/server.js
+++ webserver/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+require('dotenv').config()
 
 const app = express();
 
@@ -9,12 +10,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // ThingSpeak Configuration
-const TALKBACK_ID = "";
-const API_KEY = "";
+const TALKBACK_ID = process.env.TALKBACK_ID;
+const TALKBACK_API_KEY = process.env.TALKBACK_API_KEY;
+const READ_API_KEY = process.env.READ_API_KEY;
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+
+app.get("/data", async (req, res) => {
+  try {
+    const response = await fetch(`https://api.thingspeak.com/channels/3272963/feeds.json?api_key=${READ_API_KEY}&results=1`, {
+      method: "GET",
+    })
+    const data = await response.json()
+    res.json(data)
+  } catch (err) {
+    res.json({ error: true })
+  }
+})
 
 app.post("/sendCommand", async (req, res) => {
   const command = req.body.command;
@@ -23,7 +37,7 @@ app.post("/sendCommand", async (req, res) => {
     const response = await fetch(`https://api.thingspeak.com/talkbacks/${TALKBACK_ID}/commands.json`, {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: `api_key=${API_KEY}&command_string=SET_CO2|${encodeURIComponent(command)}`
+      body: `api_key=${TALKBACK_API_KEY}&command_string=SET_CO2|${encodeURIComponent(command)}`
     });
 
     const data = await response.json();
